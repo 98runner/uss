f21925b2f49540d74e8cbb3ffcd10770ff08fd31
98runner <159646048+98runner@users.noreply.github.com>
2024-02-24 22:54:42 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('What is your name?: ')
+age = int(input('What is your age?: '))
+location = input('Where do you live?: ')
+
+print('Hello {name}') 
+print('COD ltd wishes you a fantastic birthday as you turn '+str(age)+' years old')
+print(' Your gift will be sent to {location}')
